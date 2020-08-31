393a6f85b699936fb239f0cd6b48dcefbead1971
SidWolfie <SidWolfie@users.noreply.github.com>
2020-08-31 22:38:22 +0530

    GFG

---
+++ GFG/P1.cpp
@@ -0,0 +1,41 @@
+/* Find pairs from array whose sum is equal to a given number.  */
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a, i, j, sum, n, s=0;
+
+    cout << "Enter Size Of 1-D Array : ";
+    cin >> n ;
+
+    cout << "Enter Array : " << endl;
+
+    int A[n];
+    for (i = 0; i < n; i++)
+    {
+        cin >> A[i];
+    }
+
+    cout << "Enter A No. : " ;
+    cin >> a ;
+
+    for (i = 0; i < n; i++)
+    {
+        for (j = i+1; j < n; j++)
+        {
+            if (A[i] + A[j]==a)
+            {
+                cout << "(" << A[i] << "," << A[j] << ")" << endl;
+                s=1;
+            }          
+        }
+    }
+
+    if (s==0)
+        {
+            cout << "No Such Pair." << endl;
+        }
+} 
+
